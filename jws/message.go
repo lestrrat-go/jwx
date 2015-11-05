77cae60811d99731b308008274783986b680d7b6
Daisuke Maki <lestrrat+github@gmail.com>
2015-11-05 18:47:57 +0900

    Rmove unused struct

---
+++ jws/message.go
@@ -0,0 +1,24 @@
+package jws
+
+import "errors"
+
+func (m Message) Verify(v Verifier) error {
+	p, err := m.Payload.Base64Encode()
+	if err != nil {
+		return err
+	}
+
+	for _, sig := range m.Signatures {
+		h, err := sig.Header.Base64Encode()
+		if err != nil {
+			return err
+		}
+
+		buf := append(append(h, '.'), p...)
+		if err := v.Verify(buf, sig.Signature); err == nil {
+			return nil
+		}
+	}
+
+	return errors.New("none of the signatures could be verified")
+}
