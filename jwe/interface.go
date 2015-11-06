8650c21994bfad2f13dc6079c1cec72fe5038471
Daisuke Maki <lestrrat+github@gmail.com>
2015-11-06 15:35:19 +0900

    Start with JWE

---
+++ jwe/interface.go
@@ -0,0 +1,49 @@
+package jwe
+
+import (
+	"errors"
+	"net/url"
+
+	"github.com/lestrrat/go-jwx/buffer"
+	"github.com/lestrrat/go-jwx/jwa"
+	"github.com/lestrrat/go-jwx/jwk"
+)
+
+var ErrInvalidCompactPartsCount = errors.New("compact JWE format must have five parts")
+
+type EssentialHeader struct {
+	Algorithm              jwa.KeyEncryptionAlgorithm     `json:"alg"`
+	ContentEncryption      jwa.ContentEncryptionAlgorithm `json:"enc"`
+	ContentType            string                         `json:"cty,omitempty"`
+	Compression            jwa.CompressionAlgorithm       `json:"zip,omitempty"`
+	Critical               []string                       `json:"crit,omitempty"`
+	Jwk                    jwk.JSONWebKey                 `json:"jwk,omitempty"` // public key
+	JwkSetUrl              *url.URL                       `json:"jku,omitempty"`
+	KeyID                  string                         `json:"kid,omitempty"`
+	Type                   string                         `json:"typ,omitempty"` // e.g. "JWT"
+	X509Url                *url.URL                       `json:"x5u,omitempty"`
+	X509CertChain          []string                       `json:"x5c,omitempty"`
+	X509CertThumbprint     string                         `json:"x5t,omitempty"`
+	X509CertThumbprintS256 string                         `json:"x5t#S256,omitempty"`
+}
+
+// Header represents a jws header.
+type Header struct {
+	*EssentialHeader `json:"-"`
+	PrivateParams    map[string]interface{} `json:"-"`
+}
+
+type Recipient struct {
+	Header       Header        `json:"header"`
+	EncryptedKey buffer.Buffer `json:"encrypted_key"`
+}
+
+type Message struct {
+	AdditionalAuthenticatedData buffer.Buffer `json:"aad"`
+	CipherText                  buffer.Buffer `json:"ciphertext"`
+	InitializationVector        buffer.Buffer `json:"iv,omitempty"`
+	Protected                   buffer.Buffer `json:"protected,omitempty"`
+	Recipients                  []Recipient   `json:"recipients"`
+	AuthenticationTag           buffer.Buffer `json:"tag,omitempty"`
+	Unprotected                 string        `json:"unprotected,omitempty"`
+}
---
+++ jwe/jwe.go
@@ -0,0 +1,179 @@
+// jwe implements JWE https://tools.ietf.org/html/rfc7516
+
+package jwe
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"net/url"
+
+	"github.com/lestrrat/go-jwx/buffer"
+	"github.com/lestrrat/go-jwx/emap"
+	"github.com/lestrrat/go-jwx/jwa"
+)
+
+func NewHeader() *Header {
+	return &Header{
+		EssentialHeader: &EssentialHeader{},
+		PrivateParams:   map[string]interface{}{},
+	}
+}
+
+func (h Header) MarshalJSON() ([]byte, error) {
+	return emap.MergeMarshal(h.EssentialHeader, h.PrivateParams)
+}
+
+func (h *Header) UnmarshalJSON(data []byte) error {
+	if h.EssentialHeader == nil {
+		h.EssentialHeader = &EssentialHeader{}
+	}
+	if h.PrivateParams == nil {
+		h.PrivateParams = map[string]interface{}{}
+	}
+	return emap.MergeUnmarshal(data, h.EssentialHeader, &h.PrivateParams)
+}
+
+func (h *EssentialHeader) Construct(m map[string]interface{}) error {
+	r := emap.Hmap(m)
+	{
+		alg, err := r.GetString("alg")
+		if err != nil {
+			return err
+		}
+		h.Algorithm = jwa.KeyEncryptionAlgorithm(alg)
+	}
+
+	{
+		alg, err := r.GetString("enc")
+		if err != nil {
+			return err
+		}
+		h.ContentEncryption = jwa.ContentEncryptionAlgorithm(alg)
+	}
+	h.ContentType, _ = r.GetString("cty")
+	h.KeyID, _ = r.GetString("kid")
+	h.Type, _ = r.GetString("typ")
+	h.X509CertThumbprint, _ = r.GetString("x5t")
+	h.X509CertThumbprintS256, _ = r.GetString("x5t#256")
+	if v, err := r.GetStringSlice("crit"); err != nil {
+		h.Critical = v
+	}
+	if v, err := r.GetStringSlice("x5c"); err != nil {
+		h.X509CertChain = v
+	}
+	if v, err := r.GetString("jku"); err == nil {
+		u, err := url.Parse(v)
+		if err == nil {
+			h.JwkSetUrl = u
+		}
+	}
+
+	if v, err := r.GetString("x5u"); err == nil {
+		u, err := url.Parse(v)
+		if err == nil {
+			h.X509Url = u
+		}
+	}
+
+	return nil
+}
+
+func Parse(buf []byte) (*Message, error) {
+	buf = bytes.TrimSpace(buf)
+	if len(buf) == 0 {
+		return nil, errors.New("empty buffer")
+	}
+
+	if buf[0] == '{' {
+		return ParseJSON(buf)
+	}
+	return ParseCompact(buf)
+}
+
+func ParseJSON(buf []byte) (*Message, error) {
+	m := struct {
+		*Message
+		*Recipient
+	}{}
+
+	if err := json.Unmarshal(buf, &m); err != nil {
+		return nil, err
+	}
+
+	// if the "signature" field exist, treat it as a flattened
+	if m.Recipient != nil {
+		if len(m.Message.Recipients) != 0 {
+			return nil, errors.New("invalid message: mixed flattened/full json serialization")
+		}
+
+		m.Message.Recipients = []Recipient{*m.Recipient}
+	}
+
+	return m.Message, nil
+}
+
+func ParseCompact(buf []byte) (*Message, error) {
+	parts := bytes.Split(buf, []byte{'.'})
+	if len(parts) != 5 {
+		return nil, ErrInvalidCompactPartsCount
+	}
+
+	enc := base64.RawURLEncoding
+	p0Len := enc.DecodedLen(len(parts[0]))
+	p1Len := enc.DecodedLen(len(parts[1]))
+	p2Len := enc.DecodedLen(len(parts[2]))
+	p3Len := enc.DecodedLen(len(parts[3]))
+	p4Len := enc.DecodedLen(len(parts[4]))
+
+	out := make([]byte, p0Len+p1Len+p2Len+p3Len+p4Len)
+
+	hdrbuf := buffer.Buffer(out[:p0Len])
+	if _, err := enc.Decode(hdrbuf, parts[0]); err != nil {
+		return nil, err
+	}
+	hdrbuf = bytes.TrimRight(hdrbuf, "\x00")
+
+	hdr := NewHeader()
+	if err := json.Unmarshal(hdrbuf, hdr); err != nil {
+		return nil, err
+	}
+
+	enckeybuf := buffer.Buffer(out[p0Len : p0Len+p1Len])
+	if _, err := enc.Decode(enckeybuf, parts[1]); err != nil {
+		return nil, err
+	}
+	enckeybuf = bytes.TrimRight(enckeybuf, "\x00")
+
+	ivbuf := buffer.Buffer(out[p0Len+p1Len : p0Len+p1Len+p2Len])
+	if _, err := enc.Decode(ivbuf, parts[2]); err != nil {
+		return nil, err
+	}
+	ivbuf = bytes.TrimRight(ivbuf, "\x00")
+
+	ctbuf := buffer.Buffer(out[p0Len+p1Len+p2Len : p0Len+p1Len+p2Len+p3Len])
+	if _, err := enc.Decode(ctbuf, parts[3]); err != nil {
+		return nil, err
+	}
+	ctbuf = bytes.TrimRight(ctbuf, "\x00")
+
+	tagbuf := buffer.Buffer(out[p0Len+p1Len+p2Len+p3Len : p0Len+p1Len+p2Len+p3Len+p4Len])
+	if _, err := enc.Decode(tagbuf, parts[4]); err != nil {
+		return nil, err
+	}
+	tagbuf = bytes.TrimRight(tagbuf, "\x00")
+
+	m := &Message{
+		AuthenticationTag:    tagbuf,
+		CipherText:           ctbuf,
+		InitializationVector: ivbuf,
+		Recipients: []Recipient{
+			Recipient{
+				Header:       *hdr,
+				EncryptedKey: enckeybuf,
+			},
+		},
+	}
+	return m, nil
+}
