667353d96074ac319e609fdc50512b9497d9d4c0
Daisuke Maki <lestrrat+github@gmail.com>
2015-11-06 18:29:17 +0900

    Tweak docs

--- jwx.go
+++ jwx.go
@@ -1,12 +1,17 @@
-// jwx contains tools that deal with the various JWx technologies such as
+// Package jwx contains tools that deal with the various JWx technologies such as
 // JWT, JWS, JWE, etc in Go.
+//
+//    JWA (https://tools.ietf.org/html/rfc7518)
+//    JWE (https://tools.ietf.org/html/rfc7516)
+//    JWK (https://tools.ietf.org/html/rfc7517)
+//    JWS (https://tools.ietf.org/html/rfc7515)
 //
 // The primary focus of this library tool set is to implement the extremely
 // flexible OAuth2 / OpenID Connect protocols. There are many other libraries
 // out there that deal with all or parts of these JWx technologies:
 //
 //    https://github.com/square/go-jose
-//	  https://github.com/coreos/oidc
+//    https://github.com/coreos/oidc
 //    https://golang.org/x/oauth2
 //
 package jwx
