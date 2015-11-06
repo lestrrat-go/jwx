f160ebe22b0b219bd55081f887d1da842a9eb5b8
Daisuke Maki <lestrrat+github@gmail.com>
2015-11-06 14:56:34 +0900

    Add more constants

--- jwa/interface.go
+++ jwa/interface.go
@@ -18,3 +18,43 @@ const (
 	PS384                          = "PS384" // RSASSA-PSS using SHA384 and MGF1-SHA384
 	PS512                          = "PS512" // RSASSA-PSS using SHA512 and MGF1-SHA512
 )
+
+type KeyEncryptionAlgorithm string
+
+const (
+	RSA1_5             KeyEncryptionAlgorithm = "RSA1_5"             // RSA-PKCS1v1.5
+	RSA_OAEP                                  = "RSA-OAEP"           // RSA-OAEP-SHA1
+	RSA_OAEP_256                              = "RSA-OAEP-256"       // RSA-OAEP-SHA256
+	A128KW                                    = "A128KW"             // AES key wrap (128)
+	A192KW                                    = "A192KW"             // AES key wrap (192)
+	A256KW                                    = "A256KW"             // AES key wrap (256)
+	DIRECT                                    = "dir"                // Direct encryption
+	ECDH_ES                                   = "ECDH-ES"            // ECDH-ES
+	ECDH_ES_A128KW                            = "ECDH-ES+A128KW"     // ECDH-ES + AES key wrap (128)
+	ECDH_ES_A192KW                            = "ECDH-ES+A192KW"     // ECDH-ES + AES key wrap (192)
+	ECDH_ES_A256KW                            = "ECDH-ES+A256KW"     // ECDH-ES + AES key wrap (256)
+	A128GCMKW                                 = "A128GCMKW"          // AES-GCM key wrap (128)
+	A192GCMKW                                 = "A192GCMKW"          // AES-GCM key wrap (192)
+	A256GCMKW                                 = "A256GCMKW"          // AES-GCM key wrap (256)
+	PBES2_HS256_A128KW                        = "PBES2-HS256+A128KW" // PBES2 + HMAC-SHA256 + AES key wrap (128)
+	PBES2_HS384_A192KW                        = "PBES2-HS384+A192KW" // PBES2 + HMAC-SHA384 + AES key wrap (192)
+	PBES2_HS512_A256KW                        = "PBES2-HS512+A256KW" // PBES2 + HMAC-SHA512 + AES key wrap (256)
+)
+
+type ContentEncryptionAlgorithm string
+
+const (
+	A128CBC_HS256 ContentEncryptionAlgorithm = "A128CBC-HS256" // AES-CBC + HMAC-SHA256 (128)
+	A192CBC_HS384                            = "A192CBC-HS384" // AES-CBC + HMAC-SHA384 (192)
+	A256CBC_HS512                            = "A256CBC-HS512" // AES-CBC + HMAC-SHA512 (256)
+	A128GCM                                  = "A128GCM"       // AES-GCM (128)
+	A192GCM                                  = "A192GCM"       // AES-GCM (192)
+	A256GCM                                  = "A256GCM"       // AES-GCM (256)
+)
+
+type CompressionAlgorithm string
+
+const (
+	NoCompression CompressionAlgorithm = ""    // No compression
+	Deflate                            = "DEF" // DEFLATE (RFC 1951)
+)
