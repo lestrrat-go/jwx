668a1084d487df3a2001a994eb587687d17ca24a
Daisuke Maki <lestrrat+github@gmail.com>
2015-11-08 20:33:42 +0900

    const (...) wasn't the same for iota and non-iota declarations

--- jwa/interface.go
+++ jwa/interface.go
@@ -6,18 +6,18 @@ type SignatureAlgorithm string
 
 const (
 	NoSignature SignatureAlgorithm = "none"
-	HS256                          = "HS256" // HMAC using SHA-256
-	HS384                          = "HS384" // HMAC using SHA-384
-	HS512                          = "HS512" // HMAC using SHA-512
-	RS256                          = "RS256" // RSASSA-PKCS-v1.5 using SHA-256
-	RS384                          = "RS384" // RSASSA-PKCS-v1.5 using SHA-384
-	RS512                          = "RS512" // RSASSA-PKCS-v1.5 using SHA-512
-	ES256                          = "ES256" // ECDSA using P-256 and SHA-256
-	ES384                          = "ES384" // ECDSA using P-384 and SHA-384
-	ES512                          = "ES512" // ECDSA using P-521 and SHA-512
-	PS256                          = "PS256" // RSASSA-PSS using SHA256 and MGF1-SHA256
-	PS384                          = "PS384" // RSASSA-PSS using SHA384 and MGF1-SHA384
-	PS512                          = "PS512" // RSASSA-PSS using SHA512 and MGF1-SHA512
+	HS256       SignatureAlgorithm = "HS256" // HMAC using SHA-256
+	HS384       SignatureAlgorithm = "HS384" // HMAC using SHA-384
+	HS512       SignatureAlgorithm = "HS512" // HMAC using SHA-512
+	RS256       SignatureAlgorithm = "RS256" // RSASSA-PKCS-v1.5 using SHA-256
+	RS384       SignatureAlgorithm = "RS384" // RSASSA-PKCS-v1.5 using SHA-384
+	RS512       SignatureAlgorithm = "RS512" // RSASSA-PKCS-v1.5 using SHA-512
+	ES256       SignatureAlgorithm = "ES256" // ECDSA using P-256 and SHA-256
+	ES384       SignatureAlgorithm = "ES384" // ECDSA using P-384 and SHA-384
+	ES512       SignatureAlgorithm = "ES512" // ECDSA using P-521 and SHA-512
+	PS256       SignatureAlgorithm = "PS256" // RSASSA-PSS using SHA256 and MGF1-SHA256
+	PS384       SignatureAlgorithm = "PS384" // RSASSA-PSS using SHA384 and MGF1-SHA384
+	PS512       SignatureAlgorithm = "PS512" // RSASSA-PSS using SHA512 and MGF1-SHA512
 )
 
 // KeyEncryptionAlgorithm represents the various encryption
@@ -26,22 +26,22 @@ type KeyEncryptionAlgorithm string
 
 const (
 	RSA1_5             KeyEncryptionAlgorithm = "RSA1_5"             // RSA-PKCS1v1.5
-	RSA_OAEP                                  = "RSA-OAEP"           // RSA-OAEP-SHA1
-	RSA_OAEP_256                              = "RSA-OAEP-256"       // RSA-OAEP-SHA256
-	A128KW                                    = "A128KW"             // AES key wrap (128)
-	A192KW                                    = "A192KW"             // AES key wrap (192)
-	A256KW                                    = "A256KW"             // AES key wrap (256)
-	DIRECT                                    = "dir"                // Direct encryption
-	ECDH_ES                                   = "ECDH-ES"            // ECDH-ES
-	ECDH_ES_A128KW                            = "ECDH-ES+A128KW"     // ECDH-ES + AES key wrap (128)
-	ECDH_ES_A192KW                            = "ECDH-ES+A192KW"     // ECDH-ES + AES key wrap (192)
-	ECDH_ES_A256KW                            = "ECDH-ES+A256KW"     // ECDH-ES + AES key wrap (256)
-	A128GCMKW                                 = "A128GCMKW"          // AES-GCM key wrap (128)
-	A192GCMKW                                 = "A192GCMKW"          // AES-GCM key wrap (192)
-	A256GCMKW                                 = "A256GCMKW"          // AES-GCM key wrap (256)
-	PBES2_HS256_A128KW                        = "PBES2-HS256+A128KW" // PBES2 + HMAC-SHA256 + AES key wrap (128)
-	PBES2_HS384_A192KW                        = "PBES2-HS384+A192KW" // PBES2 + HMAC-SHA384 + AES key wrap (192)
-	PBES2_HS512_A256KW                        = "PBES2-HS512+A256KW" // PBES2 + HMAC-SHA512 + AES key wrap (256)
+	RSA_OAEP           KeyEncryptionAlgorithm = "RSA-OAEP"           // RSA-OAEP-SHA1
+	RSA_OAEP_256       KeyEncryptionAlgorithm = "RSA-OAEP-256"       // RSA-OAEP-SHA256
+	A128KW             KeyEncryptionAlgorithm = "A128KW"             // AES key wrap (128)
+	A192KW             KeyEncryptionAlgorithm = "A192KW"             // AES key wrap (192)
+	A256KW             KeyEncryptionAlgorithm = "A256KW"             // AES key wrap (256)
+	DIRECT             KeyEncryptionAlgorithm = "dir"                // Direct encryption
+	ECDH_ES            KeyEncryptionAlgorithm = "ECDH-ES"            // ECDH-ES
+	ECDH_ES_A128KW     KeyEncryptionAlgorithm = "ECDH-ES+A128KW"     // ECDH-ES + AES key wrap (128)
+	ECDH_ES_A192KW     KeyEncryptionAlgorithm = "ECDH-ES+A192KW"     // ECDH-ES + AES key wrap (192)
+	ECDH_ES_A256KW     KeyEncryptionAlgorithm = "ECDH-ES+A256KW"     // ECDH-ES + AES key wrap (256)
+	A128GCMKW          KeyEncryptionAlgorithm = "A128GCMKW"          // AES-GCM key wrap (128)
+	A192GCMKW          KeyEncryptionAlgorithm = "A192GCMKW"          // AES-GCM key wrap (192)
+	A256GCMKW          KeyEncryptionAlgorithm = "A256GCMKW"          // AES-GCM key wrap (256)
+	PBES2_HS256_A128KW KeyEncryptionAlgorithm = "PBES2-HS256+A128KW" // PBES2 + HMAC-SHA256 + AES key wrap (128)
+	PBES2_HS384_A192KW KeyEncryptionAlgorithm = "PBES2-HS384+A192KW" // PBES2 + HMAC-SHA384 + AES key wrap (192)
+	PBES2_HS512_A256KW KeyEncryptionAlgorithm = "PBES2-HS512+A256KW" // PBES2 + HMAC-SHA512 + AES key wrap (256)
 )
 
 // ContentEncryptionAlgorithm represents the various encryption
@@ -50,11 +50,11 @@ type ContentEncryptionAlgorithm string
 
 const (
 	A128CBC_HS256 ContentEncryptionAlgorithm = "A128CBC-HS256" // AES-CBC + HMAC-SHA256 (128)
-	A192CBC_HS384                            = "A192CBC-HS384" // AES-CBC + HMAC-SHA384 (192)
-	A256CBC_HS512                            = "A256CBC-HS512" // AES-CBC + HMAC-SHA512 (256)
-	A128GCM                                  = "A128GCM"       // AES-GCM (128)
-	A192GCM                                  = "A192GCM"       // AES-GCM (192)
-	A256GCM                                  = "A256GCM"       // AES-GCM (256)
+	A192CBC_HS384 ContentEncryptionAlgorithm = "A192CBC-HS384" // AES-CBC + HMAC-SHA384 (192)
+	A256CBC_HS512 ContentEncryptionAlgorithm = "A256CBC-HS512" // AES-CBC + HMAC-SHA512 (256)
+	A128GCM       ContentEncryptionAlgorithm = "A128GCM"       // AES-GCM (128)
+	A192GCM       ContentEncryptionAlgorithm = "A192GCM"       // AES-GCM (192)
+	A256GCM       ContentEncryptionAlgorithm = "A256GCM"       // AES-GCM (256)
 )
 
 // CompressionAlgorithm represents the compression algorithms
@@ -63,5 +63,5 @@ type CompressionAlgorithm string
 
 const (
 	NoCompression CompressionAlgorithm = ""    // No compression
-	Deflate                            = "DEF" // DEFLATE (RFC 1951)
+	Deflate       CompressionAlgorithm = "DEF" // DEFLATE (RFC 1951)
 )
