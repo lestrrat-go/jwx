78f451ca2503257123995ce2b2e984b3f56d096e
Daisuke Maki <lestrrat+github@gmail.com>
2015-11-09 09:38:33 +0900

    Add tests for Appendix A.2

---
+++ internal/rsautil/rsautil.go
@@ -0,0 +1,91 @@
+package rsautil
+
+import (
+	"crypto/rsa"
+	"encoding/json"
+	"math/big"
+
+	"github.com/lestrrat/go-jwx/buffer"
+)
+
+type rawkey struct {
+	N  buffer.Buffer `json:"n"`
+	E  buffer.Buffer `json:"e"`
+	D  buffer.Buffer `json:"d"`
+	P  buffer.Buffer `json:"p"`
+	Q  buffer.Buffer `json:"q"`
+	Dp buffer.Buffer `json:"dp"`
+	Dq buffer.Buffer `json:"dq"`
+	Qi buffer.Buffer `json:"qi"`
+}
+
+func NewRawKeyFromPublicKey(pubkey *rsa.PublicKey) *rawkey {
+	r := &rawkey{}
+	r.N = buffer.Buffer(pubkey.N.Bytes())
+	r.E = buffer.FromUint(uint64(pubkey.E))
+	return r
+}
+
+func NewRawKeyFromPrivateKey(privkey *rsa.PrivateKey) *rawkey {
+	r := NewRawKeyFromPublicKey(&privkey.PublicKey)
+	r.D = buffer.Buffer(privkey.D.Bytes())
+	r.P = buffer.Buffer(privkey.Primes[0].Bytes())
+	r.Q = buffer.Buffer(privkey.Primes[1].Bytes())
+	r.Dp = buffer.Buffer(privkey.Precomputed.Dp.Bytes())
+	r.Dq = buffer.Buffer(privkey.Precomputed.Dq.Bytes())
+	r.Qi = buffer.Buffer(privkey.Precomputed.Qinv.Bytes())
+	return r
+}
+
+func PublicKeyFromJSON(data []byte) (*rsa.PublicKey, error) {
+	r := rawkey{}
+	if err := json.Unmarshal(data, &r); err != nil {
+		return nil, err
+	}
+
+	return r.GeneratePublicKey()
+}
+
+func PrivateKeyFromJSON(data []byte) (*rsa.PrivateKey, error) {
+	r := rawkey{}
+	if err := json.Unmarshal(data, &r); err != nil {
+		return nil, err
+	}
+
+	return r.GeneratePrivateKey()
+}
+
+func (r rawkey) GeneratePublicKey() (*rsa.PublicKey, error) {
+	return &rsa.PublicKey{
+		N: (&big.Int{}).SetBytes(r.N.Bytes()),
+		E: int((&big.Int{}).SetBytes(r.E.Bytes()).Int64()),
+	}, nil
+}
+
+func (r rawkey) GeneratePrivateKey() (*rsa.PrivateKey, error) {
+	pubkey, err := r.GeneratePublicKey()
+	if err != nil {
+		return nil, err
+	}
+
+	privkey := &rsa.PrivateKey{
+		PublicKey: *pubkey,
+		D:         (&big.Int{}).SetBytes(r.D.Bytes()),
+		Primes: []*big.Int{
+			(&big.Int{}).SetBytes(r.P.Bytes()),
+			(&big.Int{}).SetBytes(r.Q.Bytes()),
+		},
+	}
+
+	if r.Dp.Len() > 0 {
+		privkey.Precomputed.Dp = (&big.Int{}).SetBytes(r.Dp.Bytes())
+	}
+	if r.Dq.Len() > 0 {
+		privkey.Precomputed.Dq = (&big.Int{}).SetBytes(r.Dq.Bytes())
+	}
+	if r.Qi.Len() > 0 {
+		privkey.Precomputed.Qinv = (&big.Int{}).SetBytes(r.Qi.Bytes())
+	}
+
+	return privkey, nil
+}
--- jws/jws_test.go
+++ jws/jws_test.go
@@ -9,9 +9,13 @@ import (
 	"testing"
 
 	"github.com/lestrrat/go-jwx/buffer"
+	"github.com/lestrrat/go-jwx/internal/rsautil"
 	"github.com/lestrrat/go-jwx/jwa"
 	"github.com/stretchr/testify/assert"
 )
+
+const examplePayload = `{"iss":"joe",` + "\r\n" + ` "exp":1300819380,` + "\r\n" + ` "http://example.com/is_root":true}`
+const exampleCompactSerialization = `eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk`
 
 func TestParse_EmptyByteBuffer(t *testing.T) {
 	_, err := Parse([]byte{})
@@ -19,8 +23,6 @@ func TestParse_EmptyByteBuffer(t *testing.T) {
 		return
 	}
 }
-
-const exampleCompactSerialization = `eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk`
 
 func TestParse_CompactSerializationMissingParts(t *testing.T) {
 	incoming := strings.Join(
@@ -119,8 +121,6 @@ func TestRoundtrip_Compact(t *testing.T) {
 	}
 }
 
-const examplePayload = `{"iss":"joe",` + "\r\n" + ` "exp":1300819380,` + "\r\n" + ` "http://example.com/is_root":true}`
-
 // TestEncode_HS256Compact tests that https://tools.ietf.org/html/rfc7515#appendix-A.1 works
 func TestEncode_HS256Compact(t *testing.T) {
 	const hdr = `{"typ":"JWT",` + "\r\n" + ` "alg":"HS256"}`
@@ -163,18 +163,58 @@ func TestEncode_HS256Compact(t *testing.T) {
 	}
 }
 
-func TestParse_CompactEncoded(t *testing.T) {
-	// Appendix-A.4.1
-	s := `eyJhbGciOiJFUzUxMiJ9.UGF5bG9hZA.AdwMgeerwtHoh-l192l60hp9wAHZFVJbLfD_UxMi70cwnZOYaRI1bKPWROc-mZZqwqT2SI-KGDKB34XO0aw_7XdtAG8GaSwFKdCAPZgoXD2YBJZCPEX3xKpRwcdOO8KpEHwJjyqOgzDO7iKvU8vcnwNrmxYbSW9ERBXukOXolLzeO_Jn`
+// TestEncode_RS256Compact tests that https://tools.ietf.org/html/rfc7515#appendix-A.2 works
+func TestEncode_RS256Compact(t *testing.T) {
+	const hdr = `{"alg":"RS256"}`
+	const expected = `eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.cC4hiUPoj9Eetdgtv3hF80EGrhuB__dzERat0XF9g2VtQgr9PJbu3XOiZj5RZmh7AAuHIm4Bh-0Qc_lF5YKt_O8W2Fp5jujGbds9uJdbF9CUAr7t1dnZcAcQjbKBYNX4BAynRFdiuB--f_nZLgrnbyTyWzO75vRK5h6xBArLIARNPvkSjtQBMHlb1L07Qe7K0GarZRmB_eSN9383LcOLn6_dO--xi12jzDwusC-eOkHWEsqtFZESc6BfI7noOPqvhJ1phCnvWh6IeYI2w9QOYEUipUTI8np6LbgGY9Fs98rqVt5AXLIhWkWywlVmtVrBp0igcN_IoypGlUPQGe77Rw`
+	const jwksrc = `{
+    "kty":"RSA",
+    "n":"ofgWCuLjybRlzo0tZWJjNiuSfb4p4fAkd_wWJcyQoTbji9k0l8W26mPddxHmfHQp-Vaw-4qPCJrcS2mJPMEzP1Pt0Bm4d4QlL-yRT-SFd2lZS-pCgNMsD1W_YpRPEwOWvG6b32690r2jZ47soMZo9wGzjb_7OMg0LOL-bSf63kpaSHSXndS5z5rexMdbBYUsLA9e-KXBdQOS-UTo7WTBEMa2R2CapHg665xsmtdVMTBQY4uDZlxvb3qCo5ZwKh9kG4LT6_I5IhlJH7aGhyxXFvUK-DWNmoudF8NAco9_h9iaGNj8q2ethFkMLs91kzk2PAcDTW9gb54h4FRWyuXpoQ",
+    "e":"AQAB",
+    "d":"Eq5xpGnNCivDflJsRQBXHx1hdR1k6Ulwe2JZD50LpXyWPEAeP88vLNO97IjlA7_GQ5sLKMgvfTeXZx9SE-7YwVol2NXOoAJe46sui395IW_GO-pWJ1O0BkTGoVEn2bKVRUCgu-GjBVaYLU6f3l9kJfFNS3E0QbVdxzubSu3Mkqzjkn439X0M_V51gfpRLI9JYanrC4D4qAdGcopV_0ZHHzQlBjudU2QvXt4ehNYTCBr6XCLQUShb1juUO1ZdiYoFaFQT5Tw8bGUl_x_jTj3ccPDVZFD9pIuhLhBOneufuBiB4cS98l2SR_RQyGWSeWjnczT0QU91p1DhOVRuOopznQ",
+    "p":"4BzEEOtIpmVdVEZNCqS7baC4crd0pqnRH_5IB3jw3bcxGn6QLvnEtfdUdiYrqBdss1l58BQ3KhooKeQTa9AB0Hw_Py5PJdTJNPY8cQn7ouZ2KKDcmnPGBY5t7yLc1QlQ5xHdwW1VhvKn-nXqhJTBgIPgtldC-KDV5z-y2XDwGUc",
+    "q":"uQPEfgmVtjL0Uyyx88GZFF1fOunH3-7cepKmtH4pxhtCoHqpWmT8YAmZxaewHgHAjLYsp1ZSe7zFYHj7C6ul7TjeLQeZD_YwD66t62wDmpe_HlB-TnBA-njbglfIsRLtXlnDzQkv5dTltRJ11BKBBypeeF6689rjcJIDEz9RWdc",
+    "dp":"BwKfV3Akq5_MFZDFZCnW-wzl-CCo83WoZvnLQwCTeDv8uzluRSnm71I3QCLdhrqE2e9YkxvuxdBfpT_PI7Yz-FOKnu1R6HsJeDCjn12Sk3vmAktV2zb34MCdy7cpdTh_YVr7tss2u6vneTwrA86rZtu5Mbr1C1XsmvkxHQAdYo0",
+    "dq":"h_96-mK1R_7glhsum81dZxjTnYynPbZpHziZjeeHcXYsXaaMwkOlODsWa7I9xXDoRwbKgB719rrmI2oKr6N3Do9U0ajaHF-NKJnwgjMd2w9cjz3_-kyNlxAr2v4IKhGNpmM5iIgOS1VZnOZ68m6_pbLBSp3nssTdlqvd0tIiTHU",
+    "qi":"IYd7DHOhrWvxkwPQsRM2tOgrjbcrfvtQJipd-DlcxyVuuM9sQLdgjVk2oy26F0EmpScGLq2MowX7fhd_QJQ3ydy5cY7YIBi87w93IKLEdfnbJtoOPLUW0ITrJReOgo1cq9SbsxYawBgfp_gh6A5603k2-ZQwVK0JKSHuLFkuQ3U"
+  }`
 
-	m, err := Parse([]byte(s))
-	if !assert.NoError(t, err, "Parsing compact serialization") {
+	privkey, err := rsautil.PrivateKeyFromJSON([]byte(jwksrc))
+	if !assert.NoError(t, err, "parsing jwk should be successful") {
 		return
 	}
 
-	// TODO: verify m
-	jsonbuf, _ := json.MarshalIndent(m, "", "  ")
-	t.Logf("%s", jsonbuf)
+	sign, err := NewRsaSign(jwa.RS256, privkey)
+	if !assert.NoError(t, err, "RsaSign created successfully") {
+		return
+	}
+
+	out, err := Encode(
+		buffer.Buffer(hdr),
+		buffer.Buffer(examplePayload),
+		sign,
+	)
+	if !assert.NoError(t, err, "Encode should succeed") {
+		return
+	}
+
+	if !assert.Equal(t, expected, string(out), "generated compact serialization should match") {
+		return
+	}
+
+	msg, err := Parse(out)
+	if !assert.NoError(t, err, "Parsing compact encoded serialization succeeds") {
+		return
+	}
+
+	hdrs := msg.Signatures[0].MergedHeaders()
+	if !assert.Equal(t, hdrs.Algorithm(), jwa.RS256, "Algorithm in header matches") {
+		return
+	}
+
+	if !assert.NoError(t, Verify(buffer.Buffer(hdr), buffer.Buffer(examplePayload), msg.Signatures[0].Signature.Bytes(), sign), "Verify succeeds") {
+		return
+	}
 }
 
 func TestParse_UnsecuredCompact(t *testing.T) {
