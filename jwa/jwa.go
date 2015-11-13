554ac0468a96cc117ad93a904fe3f7557c8a6b84
Daisuke Maki <lestrrat+github@gmail.com>
2015-11-13 15:05:15 +0900

    Implement Stringer() for each type in JWA

--- jwa/jwa.go
+++ jwa/jwa.go
@@ -1,2 +1,24 @@
 // Package jwa defines the various algorithm described in https://tools.ietf.org/html/rfc7518
 package jwa
+
+func (kty KeyType) String() string {
+	return string(kty)
+}
+
+func (alg SignatureAlgorithm) String() string {
+	return string(alg)
+}
+
+func (alg KeyEncryptionAlgorithm) String() string {
+	return string(alg)
+}
+
+func (alg ContentEncryptionAlgorithm) String() string {
+	return string(alg)
+}
+
+func (alg CompressionAlgorithm) String() string {
+	return string(alg)
+}
+
+
