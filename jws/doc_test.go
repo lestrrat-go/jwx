593e23e11ba907e3499958ca9e75b0c8a46a46c4
Daisuke Maki <lestrrat+github@gmail.com>
2015-11-13 22:07:06 +0900

    Update example

--- jws/doc_test.go
+++ jws/doc_test.go
@@ -8,7 +8,41 @@ import (
 	"github.com/lestrrat/go-jwx/jwa"
 )
 
-func ExampleJWS() {
+func ExampleJWS_Compact() {
+	privkey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Printf("failed to create private key: %s", err)
+		return
+	}
+
+	buf, err := Sign(jwa.RS256, []byte("Lorem ipsum"), privkey)
+	if err != nil {
+		log.Printf("failed to sign payload: %s", err)
+		return
+	}
+
+	log.Printf("%s", buf)
+
+	parsed, err := Parse(buf)
+	if err != nil {
+		log.Printf("failed to parse JSON buffer: %s", err)
+		return
+	}
+
+	v, err := NewRsaVerify(jwa.RS256, &privkey.PublicKey)
+	if err != nil {
+		log.Printf("failed to create verifier: %s", err)
+		return
+	}
+
+	if err := v.Verify(parsed); err == nil {
+		log.Printf("failed to verify JWS message: %s", err)
+		return
+	}
+	log.Printf("message verified!")
+}
+
+func ExampleJWS_JSON() {
 	privkey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		log.Printf("failed to create private key: %s", err)
