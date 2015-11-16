30bd8a2c6e5b086dbb01b486151a4ef72fb154b7
Daisuke Maki <lestrrat+github@gmail.com>
2015-11-17 07:22:53 +0900

    Remove unused types

--- jwt/interface.go
+++ jwt/interface.go
@@ -13,16 +13,6 @@ var ErrInvalidValue = errors.New("invalid value for key")
 
 const numericDateFmt = "2006-01-02T15:04:05Z UTC"
 
-type EssentialHeader struct {
-	Type        string `json:"typ"`
-	ContentType string `json:"cty,omitempty"`
-}
-
-type Header struct {
-	*EssentialHeader `json:"-"`
-	PrivateParams    map[string]interface{} `json:"-"`
-}
-
 type EssentialClaims struct {
 	Audience   []string     `json:"aud,omitempty"` // https://tools.ietf.org/html/rfc7519#section-4.1.3
 	Expiration int64        `json:"exp,omitempty"` // https://tools.ietf.org/html/rfc7519#section-4.1.4
