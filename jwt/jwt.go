d61627deb799e0927efb01176b949a07bf2bbbaa
Daisuke Maki <lestrrat+github@gmail.com>
2018-01-30 20:53:31 +0900

    Add ParseBytes and ParseString

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -4,13 +4,25 @@
 package jwt
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
+	"strings"
 
 	"github.com/lestrrat/go-jwx/jwa"
 	"github.com/lestrrat/go-jwx/jws"
 	"github.com/pkg/errors"
 )
+
+// ParseString calls Parse with the given string
+func ParseString(s string) (*Token, error) {
+	return Parse(strings.NewReader(s))
+}
+
+// ParseString calls Parse with the given byte sequence
+func ParseBytes(s []byte) (*Token, error) {
+	return Parse(bytes.NewReader(s))
+}
 
 // Parse parses the JWT token payload and creates a new `jwt.Token` object.
 // The token must be encoded in either JSON or compact format, with a valid
@@ -35,8 +47,15 @@ func New() *Token {
 	}
 }
 
+// Sign is a convenience function to create a signed JWT token serialized in
+// compact form. `key` must match the key type required by the given
+// signature method `method`
 func (t *Token) Sign(method jwa.SignatureAlgorithm, key interface{}) ([]byte, error) {
-	buf, _ := json.Marshal(t)
+	buf, err := json.Marshal(t)
+	if err != nil {
+		return nil, errors.Wrap(err, `failed to marshal token`)
+	}
+
 	var hdr = jws.NewHeader()
 	hdr.Set(`alg`, method.String())
 	hdr.Set(`typ`, `JWT`)
