e257dc07c28129a9c9139639299c6cbcdd2ef17b
Ato Araki <ato.araki@groove-x.com>
2019-04-23 10:56:13 +0900

    Support string json field for StringList

--- jwt/interface.go
+++ jwt/interface.go
@@ -1,6 +1,8 @@
 package jwt
 
 import (
+	"encoding/json"
+	"reflect"
 	"time"
 )
 
@@ -9,4 +11,28 @@ type StringList []string
 // NumericDate represents the date format used in the 'nbf' claim
 type NumericDate struct {
 	time.Time
+}
+
+func (l *StringList) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case []interface{}:
+		list := make([]string,0,len(value))
+		for _, v := range value {
+			element, ok := v.(string)
+			if ok {
+				list = append(list, element)
+			}
+		}
+		*l = list
+	case string:
+		*l = []string{value}
+	default:
+		return &json.InvalidUnmarshalError{reflect.TypeOf(v)}
+	}
+
+	return nil
 }
