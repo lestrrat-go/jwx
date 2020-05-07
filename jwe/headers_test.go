ba6d5516769561b0860240a5c4d7f1a5bee9ac08
Daisuke Maki <lestrrat+github@gmail.com>
2020-05-07 19:51:58 +0900

    Excercise jwe header accessors

--- jwe/headers_test.go
+++ jwe/headers_test.go
@@ -2,8 +2,12 @@ package jwe_test
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
 	"testing"
 
+	"github.com/lestrrat-go/jwx/buffer"
 	"github.com/lestrrat-go/jwx/jwa"
 	"github.com/lestrrat-go/jwx/jwe"
 	"github.com/lestrrat-go/jwx/jwk"
@@ -11,34 +15,62 @@ import (
 )
 
 func TestHeaders(t *testing.T) {
-	t.Run("Set/Get", func(t *testing.T) {
-		h := jwe.NewHeaders()
+	rawKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if !assert.NoError(t, err, `ecdsa.GenerateKey should succeed`) {
+		return
+	}
+	privKey, err := jwk.New(rawKey)
+	if !assert.NoError(t, err, `jwk.New should succeed`) {
+		return
+	}
 
-		data := map[string]struct {
-			Value    interface{}
-			Expected interface{}
-		}{
-			"kid":     {Value: "kid blah"},
-			"enc":     {Value: jwa.A128GCM},
-			"cty":     {Value: "application/json"},
-			"typ":     {Value: "typ blah"},
-			"x5t":     {Value: "x5t blah"},
-			"x5t#256": {Value: "x5t#256 blah"},
-			"crit":    {Value: []string{"crit blah"}},
-			"jku":     {Value: "http://github.com/lestrrat-go/jwx"},
-			"x5u":     {Value: "http://github.com/lestrrat-go/jwx"},
+	pubKey, err := jwk.New(rawKey.PublicKey)
+	if !assert.NoError(t, err, `jwk.PublicKey should succeed`) {
+		return
+	}
+
+	data := []struct {
+		Key      string
+		Value    interface{}
+		Expected interface{}
+	}{
+		{Key: jwe.AgreementPartyUInfoKey, Value: []byte("apu foobarbaz"), Expected: buffer.Buffer("apu foobarbaz")},
+		{Key: jwe.AgreementPartyVInfoKey, Value: []byte("apv foobarbaz"), Expected: buffer.Buffer("apv foobarbaz")},
+		{Key: jwe.CompressionKey, Value: jwa.Deflate},
+		{Key: jwe.ContentEncryptionKey, Value: jwa.A128GCM},
+		{Key: jwe.ContentTypeKey, Value: "application/json"},
+		{Key: jwe.CriticalKey, Value: []string{"crit blah"}},
+		{Key: jwe.EphemeralPublicKeyKey, Value: pubKey},
+		{Key: jwe.JWKKey, Value: privKey},
+		{Key: jwe.JWKSetURLKey, Value: "http://github.com/lestrrat-go/jwx"},
+		{Key: jwe.KeyIDKey, Value: "kid blah"},
+		{Key: jwe.TypeKey, Value: "typ blah"},
+		{Key: jwe.X509CertThumbprintKey, Value: "x5t blah"},
+		{Key: jwe.X509CertThumbprintS256Key, Value: "x5t#256 blah"},
+		{Key: jwe.X509URLKey, Value: "http://github.com/lestrrat-go/jwx"},
+	}
+
+	base := jwe.NewHeaders()
+
+	t.Run("Set values", func(t *testing.T) {
+		for _, tc := range data {
+			if !assert.NoError(t, base.Set(tc.Key, tc.Value), "Headers.Set should succeed") {
+				return
+			}
 		}
+	})
 
-		for name, testcase := range data {
-			h.Set(name, testcase.Value)
-			got, ok := h.Get(name)
-			if !assert.True(t, ok, "value should exist") {
+	t.Run("Set/Get", func(t *testing.T) {
+		h := base
+		for _, tc := range data {
+			got, ok := h.Get(tc.Key)
+			if !assert.True(t, ok, "value for %s should exist", tc.Key) {
 				return
 			}
 
-			expected := testcase.Expected
+			expected := tc.Expected
 			if expected == nil {
-				expected = testcase.Value
+				expected = tc.Value
 			}
 			if !assert.Equal(t, expected, got, "value should match") {
 				return
@@ -67,7 +99,15 @@ func TestHeaders(t *testing.T) {
 
 	t.Run("Iterator", func(t *testing.T) {
 		expected := map[string]interface{}{}
-		v := jwe.NewHeaders()
+		for _, tc := range data {
+			v := tc.Value
+			if expected := tc.Expected; expected != nil {
+				v = expected
+			}
+			expected[tc.Key] = v
+		}
+
+		v := base
 		t.Run("Iterate", func(t *testing.T) {
 			seen := make(map[string]interface{})
 			for iter := v.Iterate(context.TODO()); iter.Next(context.TODO()); {
