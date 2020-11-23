dadf93ecef656f6c64dbdfb2e9e983f4f2e8f469
Daisuke Maki <lestrrat+github@gmail.com>
2020-11-23 16:15:20 +0900

    reinstate some checks

--- jwe/jwe_test.go
+++ jwe/jwe_test.go
@@ -62,8 +62,11 @@ func TestParse(t *testing.T) {
 	const s = `eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZHQ00ifQ.OKOawDo13gRp2ojaHV7LFpZcgV7T6DVZKTyKOMTYUmKoTCVJRgckCL9kiMT03JGeipsEdY3mx_etLbbWSrFr05kLzcSr4qKAq7YN7e9jwQRb23nfa6c9d-StnImGyFDbSv04uVuxIp5Zms1gNxKKK2Da14B8S4rzVRltdYwam_lDp5XnZAYpQdb76FdIKLaVmqgfwX7XWRxv2322i-vDxRfqNzo_tETKzpVLzfiwQyeyPGLBIO56YJ7eObdv0je81860ppamavo35UgoRdbYaBcoh9QcfylQr66oc6vFWXRcZ_ZT2LawVCWTIy3brGPi6UklfCpIMfIjf7iGdXKHzg.48V1_ALb6US04U3b.5eym8TW_c8SuK0ltJ3rpYIzOeDQz7TALvtu6UG9oMo4vpzs9tX_EFShS8iB7j6jiSdiwkIr3ajwQzaBtQD_A.XFBoMYUZodetZdvTiFvSkQ`
 	t.Run("Compact format", func(t *testing.T) {
 		t.Run("Normal", func(t *testing.T) {
-			_, err := jwe.Parse([]byte(s))
+			msg, err := jwe.Parse([]byte(s))
 			if !assert.NoError(t, err, "Parsing JWE is successful") {
+				return
+			}
+			if !assert.Len(t, msg.Recipients(), 1, "There is exactly 1 recipient") {
 				return
 			}
 		})
@@ -169,6 +172,10 @@ func TestParse_RSAES_OAEP_AES_GCM(t *testing.T) {
 	{
 		buf, _ := json.MarshalIndent(msg, "", "  ")
 		t.Logf("%s", buf)
+	}
+
+	if !assert.Equal(t, 1, len(msg.Recipients()), "message recipients header length is 1") {
+		return
 	}
 
 	plaintext, err := msg.Decrypt(jwa.RSA_OAEP, rawkey)
