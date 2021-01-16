b56b65c15bbaf928d5cdbbe36976c693042ce90c
Daisuke Maki <lestrrat+github@gmail.com>
2021-01-17 07:57:21 +0900

    Add benchmark for ParseString
    go test -bench . -benchmem
    goos: darwin
    goarch: amd64
    pkg: github.com/lestrrat-go/jwx/jwt
    cpu: VirtualApple @ 2.50GHz
    BenchmarkJWTParse/ParseString-8         	  164150	      6675 ns/op	    7536 B/op	      64 allocs/op
    BenchmarkJWTParse/ParseBytes-8          	  179611	      6513 ns/op	    7152 B/op	      63 allocs/op
    PASS
    ok  	github.com/lestrrat-go/jwx/jwt	11.397s

--- jwt/jwt_benchmark_test.go
+++ jwt/jwt_benchmark_test.go
@@ -22,12 +22,25 @@ func BenchmarkJWTParse(b *testing.B) {
 		b.Fatal(err)
 	}
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		t2, err := jwt.ParseBytes(signed)
-		if err != nil {
-			b.Fatal(err)
+	signedString := string(signed)
+	b.Run("ParseString", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			t2, err := jwt.ParseString(signedString)
+			if err != nil {
+				b.Fatal(err)
+			}
+			_ = t2
 		}
-		_ = t2
-	}
+	})
+	b.Run("ParseBytes", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			t2, err := jwt.ParseBytes(signed)
+			if err != nil {
+				b.Fatal(err)
+			}
+			_ = t2
+		}
+	})
 }
