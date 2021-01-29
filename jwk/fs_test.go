9a656232a45d5ea6f12ce6d85e06102717a2d6c0
Daisuke Maki <lestrrat+github@gmail.com>
2021-01-29 18:42:46 +0900

    Use fs.InMmeory instead of go:embed
    https://github.com/golang/go/issues/43980
    
    go1.16 probably won't allow us to use go:embed, so just implement
    an in memory FS

--- jwk/fs_test.go
+++ jwk/fs_test.go
@@ -3,17 +3,19 @@
 package jwk_test
 
 import (
-	"embed"
 	"testing"
-	
+
+	"github.com/lestrrat-go/jwx/internal/fs"
 	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/stretchr/testify/assert"
 )
 
-//go:embed testdata
-var testdata embed.FS
-
 func TestFS(t *testing.T) {
+	testdata, err := fs.NewInMemory("testdata")
+	if !assert.NoError(t, err, `fs.NewInMemory should succeed`) {
+		return
+	}
+
 	key, err := jwk.ReadFile("testdata/rs256.jwk", jwk.WithFS(testdata))
 	if !assert.NoError(t, err, `jwk.ReadFile + WithFS should succeed`) {
 		return
