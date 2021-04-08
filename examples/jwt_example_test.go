6ad8cfe8538be695cdaaebb39142a310da794ba2
Daisuke Maki <lestrrat+github@gmail.com>
2021-04-08 19:13:05 +0900

    Fix examples

--- examples/jwt_example_test.go
+++ examples/jwt_example_test.go
@@ -227,7 +227,9 @@ func ExampleJWT_Token() {
 
 	// OUTPUT:
 	// {
-	//   "aud": "Golang Users",
+	//   "aud": [
+  //     "Golang Users"
+  //   ],
 	//   "iat": 233431200,
 	//   "privateClaimKey": "Hello, World!",
 	//   "sub": "https://github.com/lestrrat-go/jwx/jwt"
@@ -319,7 +321,9 @@ func ExampleJWT_OpenIDToken() {
 	//     "region": "東京都",
 	//     "street_address": "芝公園 4-2-8"
 	//   },
-	//   "aud": "Golang Users",
+	//   "aud": [
+  //     "Golang Users"
+  //   ],
 	//   "iat": 233431200,
 	//   "privateClaimKey": "Hello, World!",
 	//   "sub": "https://github.com/lestrrat-go/jwx/jwt"
