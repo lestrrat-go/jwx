f9344ffeb09fdf0e564fbc231c863e236417d5d7
Daisuke Maki <lestrrat+github@gmail.com>
2021-05-31 17:37:44 +0900

    Use stronger words of warning
    I think this is useful, but also very dangerous. here be dragons

--- jwt/options.go
+++ jwt/options.go
@@ -245,11 +245,22 @@ type typedClaimPair struct {
 // your choice. It works much like the RegisterCustomField, but the effect
 // is only applicable to the jwt.Parse function call which receives this option.
 //
-// Providing this option will slightly slow down the decoding process as it needs
-// to consult multiple definitions sources (global and local), so be careful
-// if you are decoding a large number of tokens, as the effects will stack up.
+// While this can be extremely useful, this option should be used with caution:
+// There are many caveats that your entire team/user-base needs to be aware of,
+// and therefore in general its use is discouraged. Only use it when you know
+// what you are doing, and you document its use clearly for others.
 //
-// This option will also NOT work unless the tokens themselves support such
+// First and foremost, this is a "per-object" option. Meaning that given the same
+// serialized format, it is possible to generate two objects whose internal
+// representations may differ. That is, if you parse one _WITH_ the option,
+// and the other _WITHOUT_, their internal representation may completely differ.
+// This could potentially lead to problems.
+//
+// Second, specifying this option will slightly slow down the decoding process
+// as it needs to consult multiple definitions sources (global and local), so
+// be careful if you are decoding a large number of tokens, as the effects will stack up.
+//
+// Finally, this option will also NOT work unless the tokens themselves support such
 // parsing mechanism. For example, while tokens obtained from `jwt.New()` and
 // `openid.New()` will respect this option, if you provide your own custom
 // token type, it will need to implement the TokenWithDecodeCtx interface.
