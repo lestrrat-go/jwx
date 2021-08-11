98d4e22fdf9c28d45c4a68a00bfc9d39df43fda1
Daisuke Maki <lestrrat+github@gmail.com>
2021-08-11 15:41:28 +0900

    tweak benchmarks

--- bench/jwt_benchmark_test.go
+++ bench/jwt_benchmark_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/lestrrat-go/jwx/internal/jwxtest"
 	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/lestrrat-go/jwx/jwt"
 )
 
@@ -18,10 +19,45 @@ func BenchmarkJWT(b *testing.B) {
 	if err != nil {
 		b.Fatal(err)
 	}
+	pubkey, err := jwk.PublicKeyOf(key)
+	if err != nil {
+		b.Fatal(err)
+	}
 
 	t1 := jwt.New()
 	t1.Set(jwt.IssuedAtKey, time.Now().Unix())
 	t1.Set(jwt.ExpirationKey, time.Now().Add(time.Hour).Unix())
+
+	b.Run("Serialization", func(b *testing.B) {
+		b.Run("Compact", func(b *testing.B) {
+			testcases := []Case{
+				{
+					Name: "jwt.Sign",
+					Test: func(b *testing.B) error {
+						_, err := jwt.Sign(t1, alg, key)
+						return err
+					},
+				},
+			}
+			for _, tc := range testcases {
+				tc.Run(b)
+			}
+		})
+		b.Run("JSON", func(b *testing.B) {
+			testcases := []Case{
+				{
+					Name: "json.Marshal",
+					Test: func(b *testing.B) error {
+						_, err := json.Marshal(t1)
+						return err
+					},
+				},
+			}
+			for _, tc := range testcases {
+				tc.Run(b)
+			}
+		})
+	})
 
 	b.Run("Serialization", func(b *testing.B) {
 		signedBuf, err := jwt.Sign(t1, alg, key)
@@ -35,7 +71,41 @@ func BenchmarkJWT(b *testing.B) {
 		jsonString := string(jsonBuf)
 		jsonReader := bytes.NewReader(jsonBuf)
 
-		b.Run("Sign", func(b *testing.B) {
+		b.Run("Compact (With Verify)", func(b *testing.B) {
+			testcases := []Case{
+				{
+					Name:      "jwt.ParseString",
+					SkipShort: true,
+					Test: func(b *testing.B) error {
+						_, err := jwt.ParseString(signedString, jwt.WithVerify(alg, pubkey))
+						return err
+					},
+				},
+				{
+					Name: "jwt.Parse",
+					Test: func(b *testing.B) error {
+						_, err := jwt.Parse(signedBuf, jwt.WithVerify(alg, pubkey))
+						return err
+					},
+				},
+				{
+					Name:      "jwt.ParseReader",
+					SkipShort: true,
+					Pretest: func(b *testing.B) error {
+						_, err := signedReader.Seek(0, 0)
+						return err
+					},
+					Test: func(b *testing.B) error {
+						_, err := jwt.ParseReader(signedReader, jwt.WithVerify(alg, pubkey))
+						return err
+					},
+				},
+			}
+			for _, tc := range testcases {
+				tc.Run(b)
+			}
+		})
+		b.Run("Compact (No Verify)", func(b *testing.B) {
 			testcases := []Case{
 				{
 					Name:      "jwt.ParseString",
@@ -61,13 +131,6 @@ func BenchmarkJWT(b *testing.B) {
 					},
 					Test: func(b *testing.B) error {
 						_, err := jwt.ParseReader(signedReader)
-						return err
-					},
-				},
-				{
-					Name: "jwt.Sign",
-					Test: func(b *testing.B) error {
-						_, err := jwt.Sign(t1, alg, key)
 						return err
 					},
 				},
@@ -110,13 +173,6 @@ func BenchmarkJWT(b *testing.B) {
 					Name: "json.Unmarshal",
 					Test: func(b *testing.B) error {
 						return json.Unmarshal(jsonBuf, &v)
-					},
-				},
-				{
-					Name: "json.Marshal",
-					Test: func(b *testing.B) error {
-						_, err := json.Marshal(t1)
-						return err
 					},
 				},
 			}
