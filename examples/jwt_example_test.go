501193687c318db3f9b1d0c89f76a076985de832
Caio Willian <caiow.willian@gmail.com>
2021-09-13 23:08:08 -0300

    chore: add example with import jwk

--- examples/jwt_example_test.go
+++ examples/jwt_example_test.go
@@ -165,6 +165,56 @@ func ExampleJWT_ParseWithJWKS() {
 	// OUTPUT:
 }
 
+// This example return a signed jwt
+func ExampleJWT_Sign_WithImportJWK() {
+
+	// your JWK
+	jwkStr := `{
+		"kty": "RSA",
+		"n": "mmO0OvOPQ53HRxV4eHOkTTxLVfk6zcq8KAD86gbnydYBNO_Si4Q1twyvefd58-BaO4N4NCEA97QrYm57ThKCe8agLGwWPHhxgbu_SAuYQehXxkf4sWy7Q17kGFG5k5AfQGZBqTY-YaawQqLlF6ILVbWab_AoEF4yB7pI3AnNnXs",
+		"e": "AQAB",
+		"d": "RzsrI2vONJcuIyjPzVslehEQfRkhPWOFTjuudNc8yA25vs_LZ11XXx42M-KvXIqtdvngUsTLan2w6pgowcuecX3t_2wUx0GJJgARfkN7gsWIS3CyXZBEEMjLGVU4vHt5zNE3GJKo3hb1TwEiulpL_Ix6hfcTSJpEaBWrBxjxV-E",
+		"p": "5EA0bi6ui1H1wsG85oc7i9O7UH58WPIK_ytzBWXFIwcaSFFBqqNYNnZaHFsMe4cbHSBgShWHO3UueGVgOKmB8Q",
+		"q": "rSi7CosQZmj_RFIYW10ef7XTZsdpIdOXV9-1dThAJUvkslKiTfdU7T0IYYsJ2K58ekJqdpcoKAVLB2SZVvdqKw",
+		"dp": "S9yjEHPng1qsShzGQgB0ZBbtTOWdQpq_2OuCAStACFJWA-8t2h8MNJ3FeWMxlOTkuBuIpVbeaX6bAV0ATBTaoQ",
+		"dq": "ZssMJhkh1jm0d-FoVix0Y4oUAiqUzaDnciH6faiz47AnBnkporEV-HPH2ugII1qJyKZOvzHCg-eIf84HfWoI2w",
+		"qi": "lyVz1HI2b1IjzOMENkmUTaVEO6DM6usZi3c3_MobUUM05yyBhnHtPjWzqWn1uJ_Gt5bkJDdcpfvmkPAhKWEU9Q"
+	}`
+
+	// create a new jwt
+	t := jwt.New()
+	t.Set(jwt.SubjectKey, `https://github.com/lestrrat-go/jwx/jwt`)
+	t.Set(jwt.AudienceKey, `Golang Users`)
+	t.Set(jwt.IssuedAtKey, time.Unix(500, 0))
+
+	buf, err := json.MarshalIndent(t, "", "  ")
+	if err != nil {
+		fmt.Printf("failed to generate JSON: %s\n", err)
+		return
+	}
+
+	fmt.Printf("%s\n", buf)
+
+	if v, ok := t.Get(`privateClaimKey`); ok {
+		fmt.Printf("privateClaimKey -> '%s'\n", v)
+	}
+
+	//convert jwk in bytes and return a new key
+	jwkey, err := jwk.ParseKey([]byte(jwkStr))
+
+	if err != nil {
+		log.Fatal("erro")
+	}
+
+	// signed and return a jwt
+	signed, _ := jwt.Sign(t, jwa.RS256, jwkey)
+
+	fmt.Println(string(signed[:]))
+
+	// output
+	// a signed jwt based on jwk
+}
+
 func ExampleJWT_Sign() {
 	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
