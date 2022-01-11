177685a422fa64a6e36cc16bcc31c8edab0d4959
Daisuke Maki <lestrrat+github@gmail.com>
2022-01-12 07:48:58 +0900

    rework tests for a bit more coverage

--- jws/jws_test.go
+++ jws/jws_test.go
@@ -1269,35 +1269,52 @@ func TestRFC7797(t *testing.T) {
 			return
 		}
 	})
-	t.Run("Valid payload when b64 = false and detached", func(t *testing.T) {
-		const payload = `$.02`
-		hdrs := jws.NewHeaders()
-		hdrs.Set("b64", false)
-		hdrs.Set("crit", "b64")
-
-		_, err := jws.Sign(nil, jwa.HS256, key, jws.WithHeaders(hdrs), jws.WithDetachedPayload([]byte(payload)))
-		if !assert.NoError(t, err, `jws.Sign should succeed`) {
-			return
-		}
-	})
-	t.Run("Roundtrip", func(t *testing.T) {
-		const payload = `hell0w0r1d`
-		hdrs := jws.NewHeaders()
-		hdrs.Set("b64", false)
-		hdrs.Set("crit", "b64")
-
-		signed, err := jws.Sign([]byte(payload), jwa.HS256, key, jws.WithHeaders(hdrs))
-		if !assert.NoError(t, err, `jws.Sign should succeed`) {
-			return
+	t.Run("Valid payload when b64 = false", func(t *testing.T) {
+		testcases := []struct {
+			Name     string
+			Payload  []byte
+			Detached bool
+		}{
+			{
+				Name:     `(Detached) payload contains a period`,
+				Payload:  []byte(`$.02`),
+				Detached: true,
+			},
+			{
+				Name:    `(NOT detached) payload does not contain a period`,
+				Payload: []byte(`hell0w0rld`),
+			},
 		}
 
-		verified, err := jws.Verify(signed, jwa.HS256, key, jws.WithDetachedPayload([]byte(payload)))
-		if !assert.NoError(t, err, `jws.Verify should succeed`) {
-			return
-		}
+		for _, tc := range testcases {
+			tc := tc
+			t.Run(tc.Name, func(t *testing.T) {
+				hdrs := jws.NewHeaders()
+				hdrs.Set("b64", false)
+				hdrs.Set("crit", "b64")
 
-		if !assert.Equal(t, []byte(payload), verified, `payload should match`) {
-			return
+				payload := tc.Payload
+				signOptions := []jws.SignOption{jws.WithHeaders(hdrs)}
+				var verifyOptions []jws.VerifyOption
+				if tc.Detached {
+					signOptions = append(signOptions, jws.WithDetachedPayload(payload))
+					verifyOptions = append(verifyOptions, jws.WithDetachedPayload(payload))
+					payload = nil
+				}
+				signed, err := jws.Sign(payload, jwa.HS256, key, signOptions...)
+				if !assert.NoError(t, err, `jws.Sign should succeed`) {
+					return
+				}
+
+				verified, err := jws.Verify(signed, jwa.HS256, key, verifyOptions...)
+				if !assert.NoError(t, err, `jws.Verify should succeed`) {
+					return
+				}
+
+				if !assert.Equal(t, tc.Payload, verified, `payload should match`) {
+					return
+				}
+			})
 		}
 	})
 
