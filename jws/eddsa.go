69118573cc7c95f6d8585f966976f64f8150add3
Daisuke Maki <lestrrat+github@gmail.com>
2022-01-22 10:03:23 +0900

    Work with crypto.Signers in ed25519 code path

--- jws/eddsa.go
+++ jws/eddsa.go
@@ -52,11 +52,22 @@ func (v eddsaVerifier) Verify(payload, signature []byte, key interface{}) (err e
 	}
 
 	var pubkey ed25519.PublicKey
-	if err := keyconv.Ed25519PublicKey(&pubkey, key); err != nil {
-		return errors.Wrapf(err, `failed to retrieve ed25519.PublicKey out of %T`, key)
+	signer, ok := key.(crypto.Signer)
+	if ok {
+		v := signer.Public()
+		pubkey, ok = v.(ed25519.PublicKey)
+		if !ok {
+			return errors.Errorf(`expected crypto.Signer.Public() to return ed25519.PublicKey, but got %T`, v)
+		}
+	} else {
+		if err := keyconv.Ed25519PublicKey(&pubkey, key); err != nil {
+			return errors.Wrapf(err, `failed to retrieve ed25519.PublicKey out of %T`, key)
+		}
 	}
+
 	if !ed25519.Verify(pubkey, payload, signature) {
 		return errors.New(`failed to match EdDSA signature`)
 	}
+
 	return nil
 }
