66bc60fcd0c85e84a84132f2a321ee54d7e5d7ad
Daisuke Maki <lestrrat+github@gmail.com>
2022-03-05 18:58:17 +0900

    Fix to use env var

--- tools/autodoc.pl
+++ tools/autodoc.pl
@@ -56,7 +56,7 @@ for my $filename (@files) {
 if ($has_diff) {
     system("git", "remote", "set-url", "origin", "https://github-actions:$ENV{GITHUB_TOKEN}\@github.com/$ENV{GITHUB_REPOSITORY}") == 0 or die $!;
     system("git", "config", "--global", "user.name", "$ENV{GITHUB_ACTOR}") == 0 or die $!;
-    system("git", "config", "--global", "user.email", "${GITHUB_ACTOR}\@users.noreply.github.com") == 0 or die $!;
+    system("git", "config", "--global", "user.email", "$ENV{GITHUB_ACTOR}\@users.noreply.github.com") == 0 or die $!;
     system("git", "commit", "-m", "autodoc updates", @files) == 0 or die $!;
     system("git", "push", "origin", "HEAD:$ENV{GITHUB_REF}") == 0 or die $!;
 }
