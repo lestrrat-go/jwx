3774351b6ac99fc577dc7c2f4d65ae066975f80a
Daisuke Maki <lestrrat+github@gmail.com>
2022-03-05 22:04:02 +0900

    Fix example

--- examples/jwt_parse_with_keyset_example_test.go
+++ examples/jwt_parse_with_keyset_example_test.go
@@ -32,16 +32,30 @@ func ExampleJWT_ParseWithKeySet() {
 			return
 		}
 		realKey.Set(jwk.KeyIDKey, `mykey`)
+		realKey.Set(jwk.AlgorithmKey, jwa.RS256)
 
 		// For demonstration purposes, we also create a bogus key
 		bogusKey := jwk.NewSymmetricKey()
 		bogusKey.Set(jwk.AlgorithmKey, jwa.NoSignature)
 		bogusKey.Set(jwk.KeyIDKey, "otherkey")
 
+		// Now create a key set that users will use to verity the signed serialized against
+		// Normally these keys are available somewhere like https://www.googleapis.com/oauth2/v3/certs
 		// This key set contains two keys, the first one is the correct one
-		keyset = jwk.NewSet()
-		keyset.Add(pubKey)
-		keyset.Add(bogusKey)
+
+		// We can use the jwk.PublicSetOf() utility to get a JWKS
+		// all of the public keys
+		{
+			privset := jwk.NewSet()
+			privset.Add(realKey)
+			privset.Add(bogusKey)
+			v, err := jwk.PublicSetOf(privset)
+			if err != nil {
+				fmt.Printf("failed to create public JWKS: %s\n", err)
+				return
+			}
+			keyset = v
+		}
 
 		signingKey = realKey
 	}
@@ -51,7 +65,7 @@ func ExampleJWT_ParseWithKeySet() {
 	token.Set(`foo`, `bar`)
 
 	// Sign the token and generate a JWS message
-	signed, err := jwt.Sign(token, jwt.WithKey(jwa.RS256, realKey))
+	signed, err := jwt.Sign(token, jwt.WithKey(jwa.RS256, signingKey))
 	if err != nil {
 		fmt.Printf("failed to generate signed serialized: %s\n", err)
 		return
@@ -60,34 +74,23 @@ func ExampleJWT_ParseWithKeySet() {
 	// This is what you typically get as a signed JWT from a server
 	serialized = signed
 
-	// Now create a key set that users will use to verity the signed serialized against
-	// Normally these keys are available somewhere like https://www.googleapis.com/oauth2/v3/certs
-	pubKey, err := jwk.New(privKey.PublicKey)
-	if err != nil {
-		fmt.Printf("failed to create JWK: %s\n", err)
-		return
-	}
-
-	// Remember, the key must have the proper "kid", and "alg"
-	// If your key does not have "alg", see jwt.InferAlgorithmFromKey()
-	pubKey.Set(jwk.AlgorithmKey, jwa.RS256)
-	pubKey.Set(jwk.KeyIDKey, "mykey")
-
 	// Actual verification:
 	// FINALLY. This is how you Parse and verify the serialized.
 	// Key IDs are automatically matched.
 	// There was a lot of code above, but as a consumer, below is really all you need
 	// to write in your code
-	token, err := jwt.Parse(
+	tok, err := jwt.Parse(
 		serialized,
 		// Tell the parser that you want to use this keyset
 		jwt.WithKeySet(keyset),
-		// Uncomment the following option if you know your key does not have an "alg"
-		// field (which is apparently the case for Azure tokens)
-		// jwt.InferAlgorithmFromKey(true),
+
+		// Replace the above option with the following option if you know your key
+		// does not have an "alg"/ field (which is apparently the case for Azure tokens)
+		// jwt.WithKeySet(keyset, jws.WithInferAlgorithmFromKey(true)),
 	)
 	if err != nil {
 		fmt.Printf("failed to parse serialized: %s\n", err)
 	}
-	_ = token
+	_ = tok
+	// OUTPUT:
 }
