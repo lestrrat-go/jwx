b36e02a26085a0c9ccdcdf2dad4a8e1d82b782ee
lestrrat <49281+lestrrat@users.noreply.github.com>
2022-03-07 15:24:43 +0900

    Add docs for JWS custom signer/verifier (#620)
    * Add docs for JWS custom signer/verifier
    
    * typo

--- examples/jws_example_test.go
+++ examples/jws_example_test.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/cloudflare/circl/sign/ed25519"
 	"github.com/lestrrat-go/jwx/v2/internal/base64"
 	"github.com/lestrrat-go/jwx/v2/internal/json"
 	"github.com/lestrrat-go/jwx/v2/jwa"
@@ -46,74 +45,6 @@ func ExampleJWS_VerifyWithJWKSet() {
 	// Now verify using the set.
 	if _, err := jws.Verify(signed, jws.WithKeySet(set)); err != nil {
 		fmt.Printf("Failed to verify using jwk.Set!: %s", err)
-	}
-
-	// OUTPUT:
-}
-
-type CirclEdDSASignerVerifier struct{}
-
-func NewCirclEdDSASigner() (jws.Signer, error) {
-	return &CirclEdDSASignerVerifier{}, nil
-}
-
-func NewCirclEdDSAVerifier() (jws.Verifier, error) {
-	return &CirclEdDSASignerVerifier{}, nil
-}
-
-func (s CirclEdDSASignerVerifier) Algorithm() jwa.SignatureAlgorithm {
-	return jwa.EdDSA
-}
-
-func (s CirclEdDSASignerVerifier) Sign(payload []byte, keyif interface{}) ([]byte, error) {
-	switch key := keyif.(type) {
-	case ed25519.PrivateKey:
-		return ed25519.Sign(key, payload), nil
-	default:
-		return nil, fmt.Errorf(`invalid key type %T`, keyif)
-	}
-}
-
-func (s CirclEdDSASignerVerifier) Verify(payload []byte, signature []byte, keyif interface{}) error {
-	switch key := keyif.(type) {
-	case ed25519.PublicKey:
-		if ed25519.Verify(key, payload, signature) {
-			return nil
-		}
-		return fmt.Errorf(`failed to verify EdDSA signature`)
-	default:
-		return fmt.Errorf(`invalid key type %T`, keyif)
-	}
-}
-
-func ExampleJWS_ExternalSignerVerifier() {
-	// This example shows how to register external jws.Signer / jws.Verifier for
-	// a given algorithm.
-	jws.RegisterSigner(jwa.EdDSA, jws.SignerFactoryFn(NewCirclEdDSASigner))
-	jws.RegisterVerifier(jwa.EdDSA, jws.VerifierFactoryFn(NewCirclEdDSAVerifier))
-
-	pubkey, privkey, err := ed25519.GenerateKey(rand.Reader)
-	if err != nil {
-		fmt.Printf(`failed to generate keys: %s`, err)
-		return
-	}
-
-	const payload = "Lorem Ipsum"
-	signed, err := jws.Sign([]byte(payload), jws.WithKey(jwa.EdDSA, privkey))
-	if err != nil {
-		fmt.Printf(`failed to generate signed message: %s`, err)
-		return
-	}
-
-	verified, err := jws.Verify(signed, jws.WithKey(jwa.EdDSA, pubkey))
-	if err != nil {
-		fmt.Printf(`failed to verify signed message: %s`, err)
-		return
-	}
-
-	if string(verified) != payload {
-		fmt.Printf(`got invalid payload: %s`, verified)
-		return
 	}
 
 	// OUTPUT:
