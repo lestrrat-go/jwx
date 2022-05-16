18a1dd0b84cdc79410afe08b1e413fda924784c4
Daisuke Maki <lestrrat+github@gmail.com>
2022-05-16 16:02:48 +0900

    show jwt.Sign using raw and jwk.Key

--- examples/jwt_serialize_jws_example_test.go
+++ examples/jwt_serialize_jws_example_test.go
@@ -19,19 +19,32 @@ func ExampleJWT_SerializeJWS() {
 		return
 	}
 
-	key, err := jwk.FromRaw([]byte(`abracadavra`))
+	rawKey := []byte(`abracadavra`)
+	jwkKey, err := jwk.FromRaw(rawKey)
 	if err != nil {
 		fmt.Printf("failed to create symmetric key: %s\n", err)
 		return
 	}
 
-	serialized, err := jwt.Sign(tok, jwt.WithKey(jwa.HS256, key))
-	if err != nil {
-		fmt.Printf("failed to sign token: %s\n", err)
-		return
+	// This example shows you two ways to passing keys to
+	// jwt.Sign()
+	//
+	// * The first key is the "raw" key.
+	// * The second one is a jwk.Key that represents the raw key.
+	//
+	// If this were using RSA/ECDSA keys, you would be using
+	// *rsa.PrivateKey/*ecdsa.PrivateKey as the raw key.
+	for _, key := range []interface{}{rawKey, jwkKey} {
+		serialized, err := jwt.Sign(tok, jwt.WithKey(jwa.HS256, key))
+		if err != nil {
+			fmt.Printf("failed to sign token: %s\n", err)
+			return
+		}
+
+		fmt.Printf("%s\n", serialized)
 	}
 
-	fmt.Printf("%s\n", serialized)
 	// OUTPUT:
+	// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjIzMzQzMTIwMCwiaXNzIjoiZ2l0aHViLmNvbS9sZXN0cnJhdC1nby9qd3gifQ.rTlpyVnHFWosNud7seqlsvhM8UoXUIAKfdWHySFO5Ro
 	// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjIzMzQzMTIwMCwiaXNzIjoiZ2l0aHViLmNvbS9sZXN0cnJhdC1nby9qd3gifQ.rTlpyVnHFWosNud7seqlsvhM8UoXUIAKfdWHySFO5Ro
 }
