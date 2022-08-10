aaffd9cc462ec46c3c37e395ac867a55315441ec
Daisuke Maki <lestrrat+github@gmail.com>
2022-08-11 05:41:25 +0900

    Fix typo in x5u field name

--- jwk/interface_gen.go
+++ jwk/interface_gen.go
@@ -16,7 +16,7 @@ const (
 	KeyOpsKey                 = "key_ops"
 	AlgorithmKey              = "alg"
 	KeyIDKey                  = "kid"
-	X509URLKey                = "x58"
+	X509URLKey                = "x5u"
 	X509CertChainKey          = "x5c"
 	X509CertThumbprintKey     = "x5t"
 	X509CertThumbprintS256Key = "x5t#S256"
@@ -103,7 +103,7 @@ type Key interface {
 	Algorithm() jwa.KeyAlgorithm
 	// KeyID returns `kid` of a JWK
 	KeyID() string
-	// X509URL returns `x58` of a JWK
+	// X509URL returns `x5u` of a JWK
 	X509URL() string
 	// X509CertChain returns `x5c` of a JWK
 	X509CertChain() *cert.Chain
