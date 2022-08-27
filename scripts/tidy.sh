dae8f7dfb272152e6cd33cfcf094ab2a7f51fd92
Daisuke Maki <lestrrat+github@gmail.com>
2022-08-27 18:33:39 +0900

    semi-automate updateing cmd/jwx without replace

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+set -e
+
+# cmd/jwx requires special treatment.
+# it should always depend on the latest version
+
+sha1=$(git log -n 1 --format=%H)
+pushd cmd/jwx
+go get -u github.com/lestrrat-go/jwx/v2@"$sha1"
+popd
+
 for dir in $(find . -name 'go.mod' | perl -pe 's{/go.mod$}{}'); do
 	pushd "$dir"
 	go mod tidy
