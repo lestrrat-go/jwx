d1043d8fcaf5a82c6ed0e49a6b1933db898340db
Daisuke Maki <lestrrat+github@gmail.com>
2022-08-28 13:59:09 +0900

    use the SHA provided by github

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -5,9 +5,11 @@ set -e
 # cmd/jwx requires special treatment.
 # it should always depend on the latest version
 
-sha1=$(git log -n 1 --format=%H)
+if [[ -z "$GITHUB_SHA" ]]; then
+	GITHUB_SHA=$(git log -n 1 --format=%H)
+fi
 pushd cmd/jwx
-go get -u github.com/lestrrat-go/jwx/v2@"$sha1"
+go get -u github.com/lestrrat-go/jwx/v2@"$GITHUB_SHA"
 popd
 
 for dir in $(find . -name 'go.mod' | perl -pe 's{/go.mod$}{}'); do
