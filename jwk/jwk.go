a996ff351c8bf4c4c1144c9ee3b32d25f351ea14
Shang Ding <sding3@ncsu.edu>
2023-10-13 02:13:12 -0500

    add IsPrivate function

--- jwk/jwk.go
+++ jwk/jwk.go
@@ -744,3 +744,13 @@ func Equal(k1, k2 Key) bool {
 
 	return bytes.Equal(tp1, tp2)
 }
+
+// IsPrivate tells whether the supplied key is a private or public key.  An
+// error is raised if the supplied key is not an asymmetric key.
+func IsPrivate(k Key) (bool, error) {
+	ak, ok := k.(asymmetricKey)
+	if ok {
+		return ak.isPrivate(), nil
+	}
+	return false, fmt.Errorf("%T is not an asymmetric key", k)
+}
