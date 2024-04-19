6471369d0fdb6d396f8c845bf78290ca3ebff205
lestrrat <49281+lestrrat@users.noreply.github.com>
2024-04-19 22:44:47 +0900

    Implement ability to handle PEM-formatted secp256k1 keys (#1117)
    * Work with secp256k1 keys in PEM format
    
    * Add missing files
    
    * tweak bazel files
    
    * guard against go < 1.20
    
    * protect the test from go < 1.20
    
    * Fix import path
    
    * Override ParsePKCS8PrivateKey
    
    * Make this feature available through a separate build tag
    
    * Add lots of warnings about the feature
    
    * Add missing rules

---
+++ jwk/es256k_go1.20_test.go
@@ -0,0 +1,44 @@
+//go:build jwx_es256k && jwx_secp256k1_pem && go1.20
+
+package jwk_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/decred/dcrd/dcrec/secp256k1/v4"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/stretchr/testify/require"
+)
+
+func TestES256KPem(t *testing.T) {
+	raw, err := secp256k1.GeneratePrivateKey()
+	require.NoError(t, err, `GeneratePrivateKey should succeed`)
+
+	testcases := []interface{}{raw.ToECDSA(), raw.PubKey().ToECDSA()}
+	for _, tc := range testcases {
+		t.Run(fmt.Sprintf("Marshal %T", tc), func(t *testing.T) {
+			key, err := jwk.FromRaw(tc)
+			require.NoError(t, err, `FromRaw should succeed`)
+
+			pem, err := jwk.Pem(key)
+			require.NoError(t, err, `Pem should succeed`)
+			require.NotEmpty(t, pem, `Pem should not be empty`)
+
+			parsed, err := jwk.Parse(pem, jwk.WithPEM(true))
+			require.NoError(t, err, `Parse should succeed`)
+			_ = parsed
+		})
+	}
+
+	t.Run("ParsePKCS8PrivateKey", func(t *testing.T) {
+		const src = `-----BEGIN PRIVATE KEY-----
+MIGEAgEAMBAGByqGSM49AgEGBSuBBAAKBG0wawIBAQQggS9t6iYyj9JSL+btkMEq
+pMYitWV4X+/Jg9zu3L8Ob5ShRANCAAT/YrxWHfw3e8lfDncJLLkPRbdby0L4qT95
+vyWU5lPpSwRbEAfSFR1E5RD9irkN1mCY8D1ko1PAlmHVB78pNzq4
+-----END PRIVATE KEY-----`
+		key, err := jwk.Parse([]byte(src), jwk.WithPEM(true))
+		require.NoError(t, err, `Parse should succeed`)
+		require.NotNil(t, key, `key should not be nil`)
+	})
+}
