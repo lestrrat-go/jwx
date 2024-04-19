fa396a61fc1c67b010bbc78226b2500caaff3712
Daisuke Maki <lestrrat+github@gmail.com>
2024-04-19 12:20:52 +0900

    Add missing files

---
+++ jwk/internal/x509/x509_nosecp256k1.go
@@ -0,0 +1,24 @@
+//go:build !jwx_es256k
+
+package x509
+
+import (
+	"crypto/ecdsa"
+	"crypto/x509"
+)
+
+func MarshalECPrivateKey(priv *ecdsa.PrivateKey) ([]byte, error) {
+	return x509.MarshalECPrivateKey(priv)
+}
+
+func ParseECPrivateKey(der []byte) (*ecdsa.PrivateKey, error) {
+	return x509.ParseECPrivateKey(der)
+}
+
+func MarshalPKIXPublicKey(pub any) ([]byte, error) {
+	return x509.MarshalPKIXPublicKey(pub)
+}
+
+func ParsePKIXPublicKey(der []byte) (any, error) {
+	return x509.ParsePKIXPublicKey(der)
+}
