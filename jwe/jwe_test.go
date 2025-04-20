9dd446af0c06eac799a0e08ccd66664eab438534
Nicola Murino <nicola.murino@gmail.com>
2025-04-20 10:07:51 +0200

    jwe: use strings.Cut in parseCompact

--- jwe/jwe_test.go
+++ jwe/jwe_test.go
@@ -123,6 +123,13 @@ func TestParse(t *testing.T) {
 				return
 			}
 		})
+		t.Run("Too many parts", func(t *testing.T) {
+			s2 := s + "."
+			_, err := jwe.Parse([]byte(s2))
+			if !assert.Error(t, err, `should fail to parse compact format with too many parts`) {
+				return
+			}
+		})
 	})
 	t.Run("JSON format", func(t *testing.T) {
 		msg, err := jwe.Parse([]byte(s))
@@ -1049,5 +1056,16 @@ func TestMaxDecompressBufferSize(t *testing.T) {
 				require.NoError(t, err, `jwe.Decrypt should succeed`)
 			}
 		})
+	}
+}
+
+func BenchmarkParseCompat(b *testing.B) {
+	buf := []byte(`eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZHQ00ifQ.OKOawDo13gRp2ojaHV7LFpZcgV7T6DVZKTyKOMTYUmKoTCVJRgckCL9kiMT03JGeipsEdY3mx_etLbbWSrFr05kLzcSr4qKAq7YN7e9jwQRb23nfa6c9d-StnImGyFDbSv04uVuxIp5Zms1gNxKKK2Da14B8S4rzVRltdYwam_lDp5XnZAYpQdb76FdIKLaVmqgfwX7XWRxv2322i-vDxRfqNzo_tETKzpVLzfiwQyeyPGLBIO56YJ7eObdv0je81860ppamavo35UgoRdbYaBcoh9QcfylQr66oc6vFWXRcZ_ZT2LawVCWTIy3brGPi6UklfCpIMfIjf7iGdXKHzg.48V1_ALb6US04U3b.5eym8TW_c8SuK0ltJ3rpYIzOeDQz7TALvtu6UG9oMo4vpzs9tX_EFShS8iB7j6jiSdiwkIr3ajwQzaBtQD_A.XFBoMYUZodetZdvTiFvSkQ`)
+
+	for range b.N {
+		_, err := jwe.Parse(buf)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
