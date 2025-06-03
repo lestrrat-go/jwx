91fb3321acb6e70dbd2e84cb8841679df9e92655
Daisuke Maki <lestrrat+github@gmail.com>
2025-06-03 12:35:24 +0900

    Make the underlying filter more generic

--- internal/filter/filter.go
+++ internal/filter/filter.go
@@ -3,6 +3,18 @@ package filter
 
 import "sync"
 
+// FilterLogic is an interface that defines the logic for filtering objects.
+type FilterLogic interface {
+	Apply(key string, object any) bool
+}
+
+// FilterLogicFunc is a function type that implements the FilterLogic interface.
+type FilterLogicFunc func(key string, object any) bool
+
+func (f FilterLogicFunc) Apply(key string, object any) bool {
+	return f(key, object)
+}
+
 // Filterable is an interface that must be implemented by objects that can be filtered.
 type Filterable[T any] interface {
 	// Keys returns the names of all fields in the object.
@@ -15,30 +27,27 @@ type Filterable[T any] interface {
 	Remove(string) error
 }
 
-// Apply is a standalone function that provides type-safe filtering based on field names.
-// It returns a new object with only the fields that match the specified names.
-func Apply[T Filterable[T]](object T, names []string) (T, error) {
-	return filterWith(object, names, true)
+// Apply is a standalone function that provides type-safe filtering based on
+// specified filter logic.
+// It returns a new object with only the fields that match the result of `logic.Apply`.
+func Apply[T Filterable[T]](object T, logic FilterLogic) (T, error) {
+	return filterWith(object, logic, true)
 }
 
-// Reject is a standalone function that provides type-safe filtering based on field names.
-// It returns a new object with only the fields that DO NOT match the specified names.
-func Reject[T Filterable[T]](object T, names []string) (T, error) {
-	return filterWith(object, names, false)
+// Reject is a standalone function that provides type-safe filtering based on
+// specified filter logic.
+// It returns a new object with only the fields that DO NOT match the result
+// of `logic.Apply`.
+func Reject[T Filterable[T]](object T, logic FilterLogic) (T, error) {
+	return filterWith(object, logic, false)
 }
 
 // filterWith is an internal function used by both Apply and Reject functions
 // to apply the filtering logic to an object. If include is true, only fields
-// matching the names are included. If include is false, fields matching
-// the names are excluded.
-func filterWith[T Filterable[T]](object T, names []string, include bool) (T, error) {
+// matching the logic are included. If include is false, fields matching
+// the logic are excluded.
+func filterWith[T Filterable[T]](object T, logic FilterLogic, include bool) (T, error) {
 	var zero T
-
-	// Build a map for faster lookups
-	fields := make(map[string]struct{}, len(names))
-	for _, name := range names {
-		fields[name] = struct{}{}
-	}
 
 	result, err := object.Clone()
 	if err != nil {
@@ -46,7 +55,7 @@ func filterWith[T Filterable[T]](object T, names []string, include bool) (T, err
 	}
 
 	for _, k := range result.Keys() {
-		if _, ok := fields[k]; (include && ok) || (!include && !ok) {
+		if ok := logic.Apply(k, object); (include && ok) || (!include && !ok) {
 			continue
 		}
 
@@ -58,34 +67,44 @@ func filterWith[T Filterable[T]](object T, names []string, include bool) (T, err
 	return result, nil
 }
 
+// NameBasedFilter is a filter that filters fields based on their field names.
 type NameBasedFilter[T Filterable[T]] struct {
-	names []string
+	names map[string]struct{}
 	mu    sync.RWMutex
+	logic FilterLogic
 }
 
 // NewNameBasedFilter creates a new NameBasedFilter with the specified field names.
 func NewNameBasedFilter[T Filterable[T]](names ...string) *NameBasedFilter[T] {
-	return &NameBasedFilter[T]{
-		names: names,
+	nameMap := make(map[string]struct{}, len(names))
+	for _, name := range names {
+		nameMap[name] = struct{}{}
 	}
+
+	nf := &NameBasedFilter[T]{
+		names: nameMap,
+	}
+
+	nf.logic = FilterLogicFunc(nf.filter)
+	return nf
 }
 
-// Filter returns a new object with only the fields that match the filter.
+func (nf *NameBasedFilter[T]) filter(k string, object any) bool {
+	_, ok := nf.names[k]
+	return ok
+}
+
+// Filter returns a new object with only the fields that match the specified names.
 func (nf *NameBasedFilter[T]) Filter(object T) (T, error) {
 	nf.mu.RLock()
-	names := make([]string, len(nf.names))
-	copy(names, nf.names)
-	nf.mu.RUnlock()
+	defer nf.mu.RUnlock()
 
-	return Apply(object, names)
+	return Apply(object, nf.logic)
 }
 
-// Reject returns a new object with only the fields that DO NOT match the filter.
+// Reject returns a new object with only the fields that DO NOT match the specified names.
 func (nf *NameBasedFilter[T]) Reject(object T) (T, error) {
 	nf.mu.RLock()
-	names := make([]string, len(nf.names))
-	copy(names, nf.names)
-	nf.mu.RUnlock()
-
-	return Reject(object, names)
+	defer nf.mu.RUnlock()
+	return Reject(object, nf.logic)
 }
