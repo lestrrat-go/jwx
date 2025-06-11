62622f4f21989957e17184f67dea258863a457a9
Daisuke Maki <lestrrat+github@gmail.com>
2025-06-11 20:01:08 +0900

    Add missing files

---
+++ jws/legacy.go
@@ -0,0 +1,21 @@
+package jws
+
+import (
+	"github.com/lestrrat-go/jwx/v3/jwa"
+	"github.com/lestrrat-go/jwx/v3/jws/legacy"
+)
+
+func init() {
+	for _, alg := range []jwa.SignatureAlgorithm{jwa.HS256(), jwa.HS384(), jwa.HS512()} {
+		RegisterSigner(alg, func(alg jwa.SignatureAlgorithm) SignerFactory {
+			return SignerFactoryFn(func() (Signer, error) {
+				return legacy.NewHMACSigner(alg), nil
+			})
+		}(alg))
+		RegisterVerifier(alg, func(alg jwa.SignatureAlgorithm) VerifierFactory {
+			return VerifierFactoryFn(func() (Verifier, error) {
+				return legacy.NewHMACVerifier(alg), nil
+			})
+		}(alg))
+	}
+}
