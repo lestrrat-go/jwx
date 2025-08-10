49f6f91d42ba47578e6cffa6368d4573734c4aa5
Daisuke Maki <lestrrat+github@gmail.com>
2025-08-10 12:56:15 +0900

    appease linter

--- jws/jwsbb/es256k.go
+++ jws/jwsbb/es256k.go
@@ -9,9 +9,8 @@ import (
 func init() {
 	const RFC7518Alg = "ES256K"
 	// Register mapping for ES256K to dsig algorithm
-	RegisterDsigAlgorithm(RFC7518Alg, dsigsecp256k1.ECDSAWithSecp256k1AndSHA256)
+	_ = RegisterDsigAlgorithm(RFC7518Alg, dsigsecp256k1.ECDSAWithSecp256k1AndSHA256)
 
 	// Register ES256K to use crypto.SHA256
-	RegisterECDSAHashFunc(RFC7518Alg, crypto.SHA256)
-
+	_ = RegisterECDSAHashFunc(RFC7518Alg, crypto.SHA256)
 }
