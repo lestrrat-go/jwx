b0a4af3f6adc2428512a58ca4ed5b7435b3194c9
Daisuke Maki <lestrrat+github@gmail.com>
2026-04-06 15:11:46 +0900

    move examples to ext/examples

--- ext/examples/go.mod
+++ ext/examples/go.mod
@@ -31,4 +31,4 @@ require (
 
 replace github.com/cloudflare/circl v1.0.0 => github.com/cloudflare/circl v1.0.1-0.20210104183656-96a0695de3c3
 
-replace github.com/lestrrat-go/jwx/v3 => ../
+replace github.com/lestrrat-go/jwx/v3 => ../../
--- tools/test.sh
+++ tools/test.sh
@@ -22,7 +22,7 @@ esac
 
 failures=0
 echo "mode: atomic" > "$DST"
-for dir in . ./examples ./bench/performance ./cmd/jwx; do
+for dir in . ./ext/examples ./bench/performance ./cmd/jwx; do
 	testout=$(mktemp /tmp/jwx-test.XXXXX)
 	pushd "$dir" > /dev/null
 	go test -race ${testopts[@]} ./... 
