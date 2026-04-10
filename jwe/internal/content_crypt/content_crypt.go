bacd900aac4926db3a6f3bf3012430126cc075e4
Daisuke Maki <lestrrat+github@gmail.com>
2026-04-10 09:09:56 +0900

    reduce allocations in JWE encrypt and decrypt

--- jwe/internal/content_crypt/content_crypt.go
+++ jwe/internal/content_crypt/content_crypt.go
@@ -2,10 +2,13 @@ package content_crypt //nolint:golint
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/lestrrat-go/jwx/v4/jwa"
 	"github.com/lestrrat-go/jwx/v4/jwe/internal/cipher"
 )
+
+var genericCache sync.Map // map[string]*Generic
 
 func (c Generic) Algorithm() jwa.ContentEncryptionAlgorithm {
 	return c.alg
@@ -25,17 +28,24 @@ func (c Generic) Decrypt(cek, iv, ciphertext, tag, aad []byte) ([]byte, error) {
 }
 
 func NewGeneric(alg jwa.ContentEncryptionAlgorithm) (*Generic, error) {
-	c, err := cipher.NewAES(alg.String())
+	key := alg.String()
+	if v, ok := genericCache.Load(key); ok {
+		return v.(*Generic), nil
+	}
+
+	c, err := cipher.NewAES(key)
 	if err != nil {
 		return nil, fmt.Errorf(`aes crypt: failed to create content cipher: %w`, err)
 	}
 
-	return &Generic{
+	g := &Generic{
 		alg:     alg,
 		cipher:  c,
 		keysize: c.KeySize(),
 		tagsize: 16,
-	}, nil
+	}
+	genericCache.Store(key, g)
+	return g, nil
 }
 
 func (c Generic) KeySize() int {
